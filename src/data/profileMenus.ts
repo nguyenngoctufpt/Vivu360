5c4ea36aea25300c70618ceeec2a8e56f03d23c1
DoTrungHieu2004 <hieu10productions@gmail.com>
2026-06-13 20:24:19 +0700

    Di chuyển một số chức năng từ nav drawer sang màn hình tài khoản, xóa nav drawer

---
+++ src/data/profileMenus.ts
@@ -0,0 +1,25 @@
+export const profileMenus = [
+  {
+    title: 'Lịch sử đặt chỗ',
+    icon: 'time-outline',
+  },
+  {
+    title: 'Ví của tôi',
+    icon: 'wallet-outline',
+  }
+];
+
+export const supportMenus = [
+  {
+    title: 'Hỗ trợ',
+    icon: 'headset-outline',
+  },
+  {
+    title: 'Giới thiệu ứng dụng',
+    icon: 'information-circle-outline',
+  },
+  {
+    title: 'Cài đặt',
+    icon: 'settings-outline',
+  },
+];
